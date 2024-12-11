e97e2063d362f210a6494b300270961c22ede12e
Slava Lishnevsky <slishnevsky@gmail.com>
2024-12-10 20:40:26 -0500

    will not create 32px images, using buffer

--- index.js
+++ index.js
@@ -4,72 +4,56 @@ import url from 'url';
 import sharp from 'sharp';
 
 function createBookmarklets() {
-  // Input and output file paths
-  const inputFile = 'template.html'; // Replace with your input file path
-  const outputFile = 'bookmarklets.html'; // Replace with your output file path
-
   // Build the full path to the file in the 'images' subfolder
-  const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+  const scriptFolder = path.dirname(url.fileURLToPath(import.meta.url));
+  // Define the paths to the input and output files
+  const templateFile = path.join(scriptFolder, 'template.html');
+  const bookmarkletsFile = path.join(scriptFolder, 'bookmarklets.html');
 
-  // Read the file
-  fs.readFile(inputFile, 'utf8', (err, data) => {
+  // Read the content of the input file
+  fs.readFile(templateFile, 'utf8', async (err, data) => {
     if (err) console.error('Error reading the file:', err);
 
     // Split text into lines
     let lines = data.split(/\r?\n/);
 
+    // Process each line to create bookmarklets and save them to the output file
     for (let index = 0; index < lines.length; index++) {
       // Extract the site name from the line
       const site = lines[index].split('ICON="{{base64}}">')[1].replace('</A>', '')
-      const fileName1 = `${site.toLocaleLowerCase()}-512.png`;
-      const fileName2 = `${site.toLocaleLowerCase()}-32.png`;
+      const imageFile = path.join(scriptFolder, 'images', `${site.toLocaleLowerCase()}.png`);
 
-      createRoundedCorners(fileName1, fileName2);
+      // Create a rounded corners version of the image using Sharp
+      const imageBuffer = await sharp(imageFile)
+      .resize(32) // Resize to 32x32 pixels
+      .composite([{ // Create a rounded corner rect using SVG
+        input: Buffer.from('<svg width="32" height="32"><rect x="0" y="0" width="32" height="32" rx="5" ry="5" fill="white" /></svg>'), // Input the rounded corner rect as a buffer
+        blend: 'dest-in' // Combine the original image with the rounded corner rect using the 'dest-in' blend mode
+      }])
+      .toBuffer();
 
-      const filePath = path.join(__dirname, 'images', fileName2);
-      const base64string = imageToBase64(filePath);
-      lines[index] = lines[index].replace('{{base64}}', base64string);
+      // Check if the image creation was successful
+      if (!imageBuffer) {
+        console.error(`Error creating rounded corners for ${site}`);
+        return;
+      }
+      // Convert the image buffer to a Base64 string for embedding in the HTML file
+      const base64String = `data:image/png;base64,${imageBuffer.toString('base64')}`;
+
+      // Replace the icon in the HTML file with the Base64 string
+      lines[index] = lines[index].replace('{{base64}}', base64String);
+
       console.log(`${site} bookmarklet is created successfully.`)
     }
 
+    // Join the lines back into a single string and write it to the output file
     data = lines.join('\n');
 
-    // Write the text into a file named 'output.txt'
-    fs.writeFile(outputFile, data, 'utf8', (err) => {
+    fs.writeFile(bookmarkletsFile, data, 'utf8', (err) => {
       if (err) console.error('Error writing to file:', err);
-      console.log(`${outputFile} created successfully.`);
+      console.log(`${bookmarkletsFile} created successfully.`);
     });
   });
 }
 
-// Function to convert an image file to a Base64 string
-function imageToBase64(filePath) {
-  try {
-    // Read the image file synchronously
-    const imageBuffer = fs.readFileSync(filePath);
-
-    // Convert the file buffer to a Base64 string
-    const base64String = imageBuffer.toString('base64');
-
-    return `data:image/png;base64,${base64String}`;
-  } catch (error) {
-    console.error('Error reading file:', error.message);
-  }
-}
-
-// Function to create rounded corners
-function createRoundedCorners(fileName1, fileName2) {
-  const inputImage = 'images/' + fileName1;
-  const outputImage = 'images/' + fileName2;
-  sharp(inputImage)
-    .resize(32) // Optional: Resize the image (you can skip this step if not needed)
-    .composite([{
-      input: Buffer.from('<svg width="32" height="32"><rect x="0" y="0" width="32" height="32" rx="5" ry="5" fill="white" /></svg>'),
-      blend: 'dest-in'
-    }])
-    .toFile(outputImage)
-}
-
 createBookmarklets();
-
-// console.log(imageToBase64('d:\\Projects\\Bookmarklets\\images\\kijiji-32.png'));
